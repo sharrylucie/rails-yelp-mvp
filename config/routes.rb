c6e9900586567bda3d88247a3f0d8dfb2a46155f
Lucie Moreau <luciem.moreau@gmail.com>
2018-02-21 17:18:46 +0100

    scss done on index, new restaurant and review

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :restaurants, only: [:create, :index, :show, :new] do
     resources :reviews, only: [:new, :create]
   end
+
+  root to: 'restaurants#index' 
 end
