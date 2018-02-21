99b67980b4f40a5390d005df73e209f500861b1e
Lucie Moreau <luciem.moreau@gmail.com>
2018-02-21 15:31:50 +0100

    back-end installed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+5.times do
+  restaurant = Restaurant.new(
+    name: %w(oberkampf roquette miramar ibiza bajoue).sample,
+    address: %w(12oberkampf 11roquette 10miramar 1ibiza 54bajoue).sample,
+    phone_number: "01 23 23 23 23",
+    category: %w(chinese italian japanese french belgian).sample
+  )
+  restaurant.save!
+end
